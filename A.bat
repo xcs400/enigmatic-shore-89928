0da96746b2280297e25188b042d44e493d78b299
xcs400 <xcs400.pp@gmail.com>
2021-03-23 11:47:45 +0100

    Update A.bat

--- A.bat
+++ A.bat
@@ -1,1 +1,1 @@
-node tunnel-connect.js enigmatic-shore-89928.herokuapp.com:80 forward dev71-pafil.msc.tiama.priv:1111 127.0.0.1:4444
+node tunnel-connect.js enigmatic-shore-89928.herokuapp.com:80 forward dev71-pafil.msc.tiama.priv:1111 127.0.0.1:4444 
